0e8751e99588ac11da76b707b20adf219ab87000
munoztd0 <david.munoztord@mailbox.org>
2022-04-04 18:57:58 +0200

    added form

--- js/myfunc.js
+++ js/myfunc.js
@@ -357,7 +357,7 @@ function doSomething() {
     var TotPrice = ((getTypePrice() + getDistPrice()) * getUrgPrice()) + getPoidsPrice() //+ candlesPrice() + insciptionPrice();
     var divobj = document.getElementById('totalPrice');
     divobj.style.display = 'block';
-    divobj.innerHTML = "Prix:  " + TotPrice + "  CHF";
+    divobj.innerHTML = TotPrice + "  CHF";
 
     return false;
 }
@@ -428,3 +428,79 @@ function init() {
 
     });
 }
+
+
+
+// datetime
+$('#datetimePick').datetimepicker({
+    format: 'hh:mm:ss a'
+});
+
+$('#datetimeDeli').datetimepicker({
+    format: 'hh:mm:ss a'
+});
+
+$('#dateID').datetimepicker({
+    "allowInputToggle": true,
+    "showClose": true,
+    "showClear": true,
+    "showTodayButton": true,
+    "format": "DD/MM/YYYY",
+});
+
+
+// submit
+
+
+$(function() {
+    function after_form_submitted(data) {
+        if (data.result == 'success') {
+            $('form#reused_form').hide();
+            $('#success_message').show();
+            $('#error_message').hide();
+        } else {
+            $('#error_message').append('<ul></ul>');
+
+            jQuery.each(data.errors, function(key, val) {
+                $('#error_message ul').append('<li>' + key + ':' + val + '</li>');
+            });
+            $('#success_message').hide();
+            $('#error_message').show();
+
+            //reverse the response on the button
+            $('button[type="button"]', $form).each(function() {
+                $btn = $(this);
+                label = $btn.prop('orig_label');
+                if (label) {
+                    $btn.prop('type', 'submit');
+                    $btn.text(label);
+                    $btn.prop('orig_label', '');
+                }
+            });
+
+        } //else
+    }
+
+    $('#reused_form').submit(function(e) {
+        e.preventDefault();
+
+        $form = $(this);
+        //show some response on the button
+        $('button[type="submit"]', $form).each(function() {
+            $btn = $(this);
+            $btn.prop('type', 'button');
+            $btn.prop('orig_label', $btn.text());
+            $btn.text('Sending ...');
+        });
+
+
+        $.ajax({
+            type: "POST",
+            url: 'handler.php',
+            data: $form.serialize(),
+            success: after_form_submitted,
+            dataType: 'json'
+        });
+
+    });
+});
